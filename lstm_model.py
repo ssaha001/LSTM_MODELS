b137b83cc3caf347fb63b005a42c020d48576588
ssaha001 <86736327+ssaha001@users.noreply.github.com>
2022-09-18 13:07:31 -0400

    Add files via upload

---
+++ lstm_model.py
@@ -0,0 +1,72 @@
+
+# Importing all the necessary packages
+import torch
+import numpy as np
+import torch.nn as nn
+import pandas as pd
+
+# Downloading the data and processing it.
+dataset=pd.read_csv('https://raw.githubusercontent.com/jbrownlee/Datasets/master/airline-passengers.csv')
+dataset['Month']=dataset['Month'].str.replace('-','').astype(dtype='float32')
+dataset['Passengers']=dataset['Passengers'].astype(dtype='float32')
+
+# Finding and storing the number of rows in a dataset into a constant
+NUMBER_OF_ROWS=dataset.shape[0]
+
+# Turning the data into X and Y Labels
+x=dataset['Month']
+y=dataset['Passengers']
+
+# Creating the training and test data in 4:1 ratio. 
+# Since this a very small set of data, I am not using train_test_split function of scikit-learn
+x_train_data,x_test_data=x[:int((NUMBER_OF_ROWS*4)/5)],x[int((NUMBER_OF_ROWS*4)/5):]
+y_train_data,y_test_data=y[:int((NUMBER_OF_ROWS*4)/5)],y[int((NUMBER_OF_ROWS*4)/5):]
+
+# Turning the training and test data into tensors
+x_train_data_tensor,x_test_data_tensor=torch.tensor(x_train_data.values).unsqueeze(dim=0),torch.tensor(x_test_data.values).unsqueeze(dim=0)
+y_train_data_tensor,y_test_data_tensor=torch.tensor(y_train_data.values).unsqueeze(dim=0),torch.tensor(y_test_data.values).unsqueeze(dim=0)
+
+# Defining the LSTM network
+class LSTM(nn.Module):
+    def __init__(self,input_size,hidden_size):
+        super(LSTM,self).__init__()
+        self.hidden_size=hidden_size
+        self.first_layer=nn.LSTMCell(1,self.hidden_size)
+        self.second_layer=nn.LSTMCell(self.hidden_size,self.hidden_size)
+        self.output_layer=nn.Linear(self.hidden_size,1)
+    def forward(self,x_data):
+        output_list=[]
+        sample_size=x_data.size(0)
+
+        h_t=torch.zeros(sample_size,self.hidden_size,dtype=torch.float32)
+        c_t=torch.zeros(sample_size,self.hidden_size,dtype=torch.float32)
+        h_t2=torch.zeros(sample_size,self.hidden_size,dtype=torch.float32)
+        c_t2=torch.zeros(sample_size,self.hidden_size,dtype=torch.float32)
+
+        for input_tensor in x_train_data_tensor.split(1,dim=1):
+            h_t,c_t=self.first_layer(input_tensor,(h_t,c_t))
+            h_t2,c_t2=self.second_layer(h_t,(h_t2,c_t2))
+            output=self.output_layer(h_t2)
+            output_list.append(output)
+        output_list=torch.cat(output_list,dim=1)
+        return output_list
+
+# Defining the hyperparameters
+input_size=1
+hidden_size=100
+num_epochs=5
+learning_rate=0.01
+
+model=LSTM(input_size,hidden_size)
+
+criterion=nn.MSELoss()
+optimizer=torch.optim.Adam(model.parameters(),lr=learning_rate)
+
+# Training
+for epoch in range(num_epochs):
+    predictions=model(x_train_data_tensor)
+    loss=criterion(predictions,y_train_data_tensor)
+    optimizer.zero_grad()
+    loss.backward()
+    optimizer.step()
+    print (f'Epoch [{epoch+1}/{num_epochs}], Loss: {loss.item():.4f}')
---
+++ readme.txt
@@ -0,0 +1,9 @@
+For running this python we will need to install the libraries mentioned in requirements.txt file.
+    we can use this command:
+        pip install -r requirements.txt
+I have developed two different models.
+In lstm_model.py, the month and year is combined into a single column.
+But in lstm_model1.py, the month and year are separated into two different columns. 
+To run any of the scripts, use this command
+        python <name_of_the_script>
+The loss of these predictions are huge. I think I will need to learn mode about LSTM models to get better results.
